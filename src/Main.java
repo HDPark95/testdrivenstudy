c569af8b1d05d3da3755e2bfff1e93c0eb9953d2
HDPark95 <qkrgusen456@gmail.com>
2022-01-08 12:20:41 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        testMultiplication();
+    }
+    public void testMultiplication(){
+        Dollar fieve = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
